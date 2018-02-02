f000e38d6d2dd4558558aed8489fe88db863731a
Thorn <youzijia555@outlook.com>
2018-02-02 18:07:57 +0800

    网站的初次建立

---
+++ microblog/app/views.py
@@ -0,0 +1,30 @@
+from flask import render_template, flash, redirect
+from app import app
+from .forms import LoginForm
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = { 'nickname': 'Thone' } # 虚拟用户
+    posts = [ # 虚拟文章列表
+        {
+            'author': { 'nickname': 'John' },
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': { 'nickname': 'Susan' },
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template("index.html",
+        title = 'Home',
+        user = user,
+        posts = posts)
+# index view function suppressed for brevity
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html',
+        title = 'Sign In',
+        form = form)
---
+++ what i did/class.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+
+# class Student(object):
+#     def __init__(self, name, score):
+#         self.name = name
+#         self.score = score
+
+#     def get_grade(self):
+#         if self.score >= 90:
+#             return 'A'
+#         elif self.score >= 60:
+#             return 'B'
+#         else:
+#             return 'C'
+
+# lisa = Student('Lisa', 99)
+# bart = Student('Bart', 59)
+# print(lisa.name, lisa.get_grade())
+# print(bart.name, bart.get_grade())
+
+
+
+
+
+
+# class Student(object):
+#     def __init__(self, name, gender):
+#         self.name = name
+#         self.__gender = gender
+#     def get_gender(self):
+#         return self.__gender
+#     def set_gender(self,gender):
+#         self.__gender=gender
+
+# # 测试:
+# bart = Student('Bart', 'male')
+# if bart.get_gender() != 'male':
+#     print('测试失败!')
+# else:
+#     bart.set_gender('female')
+#     if bart.get_gender() != 'female':
+#         print('测试失败!')
+#     else:
+#         print('测试成功!')
+
+
+
+
+
+
+class Student(object):
+    count = 0
+
+    def __init__(self, name):
+        self.name = name
+        Student.count= Student.count +1
+
+# 测试:
+if Student.count != 0:
+    print('测试失败!')
+else:
+    bart = Student('Bart')
+    if Student.count != 1:
+        print('测试失败!')
+    else:
+        lisa = Student('Bart')
+        if Student.count != 2:
+            print('测试失败!')
+        else:
+            print('Students:', Student.count)
+            print('测试通过!')
