e74e5456af08c5454f9d77f432f9d4a5c47509d6
Thorn <youzijia555@outlook.com>
2018-02-05 19:10:01 +0800

    1

--- microblog/app/forms.py
+++ microblog/app/forms.py
@@ -1,7 +1,8 @@
-from flask.ext.wtf import Form
-from wtforms import StringField, BooleanField
-from wtforms.validators import DataRequired
 
-class LoginForm(Form):
+from flask.ext.wtf import form
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired, Email
+
+class LoginForm(form):
     openid = StringField('openid', validators=[DataRequired()])
     remember_me = BooleanField('remember_me', default=False)
