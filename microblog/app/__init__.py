7cf7d5bc4cacdcfbf249c6a109c8702e35ecc301
Thorn <youzijia555@outlook.com>
2018-02-15 22:45:15 +0800

    2/15

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,5 +1,4 @@
 import os
-from app import views, models
 from config import basedir
 from flask import Flask
 from flask.ext.login import LoginManager
@@ -8,10 +7,12 @@ from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
+
 db = SQLAlchemy(app)
-lm = LoginManager()
-lm.init_app(app)
-oid = OpenID(app, os.path.join(basedir, 'tmp'))
+
 lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
+
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
+from app import views, models
